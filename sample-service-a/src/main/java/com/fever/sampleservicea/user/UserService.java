a155ff25d21562cda36ca903d04155ab97518789
Yu UiSeon <dmltjs851@gmail.com>
2020-05-14 12:02:07 +0900

    Update UserController, UserService

--- sample-service-a/src/main/java/com/fever/sampleservicea/user/UserService.java
+++ sample-service-a/src/main/java/com/fever/sampleservicea/user/UserService.java
@@ -1,4 +1,17 @@
 package com.fever.sampleservicea.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
 }
