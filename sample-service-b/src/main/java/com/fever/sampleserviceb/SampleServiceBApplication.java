9a33aa31910a2644bab22eba6f6f86a21cfd264f
Yu UiSeon <dmltjs851@gmail.com>
2020-05-14 10:56:48 +0900

    Add sample service
    - Gateway와 연동 테스트 하기 위한 sample api

---
+++ sample-service-b/src/main/java/com/fever/sampleserviceb/SampleServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.fever.sampleserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServiceBApplication.class, args);
+    }
+
+}
---
+++ sample-service-b/src/test/java/com/fever/sampleserviceb/SampleServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fever.sampleserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleServiceBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
