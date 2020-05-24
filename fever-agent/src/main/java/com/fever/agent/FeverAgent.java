317a3cb67991cb0c0b7f48aff5f42bd063a8017b
Yu UiSeon <dmltjs851@gmail.com>
2020-05-24 19:21:49 +0900

    Hello Fever Agent
    - Remove Spring Boot dependency

--- fever-agent/src/main/java/com/fever/agent/FeverAgent.java
+++ fever-agent/src/main/java/com/fever/agent/FeverAgent.java
@@ -1,13 +1,8 @@
 package com.fever.agent;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class FeverAgent {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeverAgent.class, args);
+        System.out.println("Hello Fever Agent !");
     }
-
 }
