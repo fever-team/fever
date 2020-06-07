c43514106bc120916b987466d47fb7087b9fd038
Yu UiSeon <dmltjs851@gmail.com>
2020-06-08 05:51:21 +0900

    Add AgentController

--- fever-controller/src/main/java/com/fever/fevercontroller/controller/AgentController.java
+++ fever-controller/src/main/java/com/fever/fevercontroller/controller/AgentController.java
@@ -1,4 +1,16 @@
 package com.fever.fevercontroller.controller;
 
+import com.fever.fevercontroller.model.AgentRequest;
+import com.fever.fevercontroller.model.ControllerRequest;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AgentController {
+
+    @MessageMapping("/controller")
+    public void controllerRequest(AgentRequest agentRequest) {
+        System.out.println("Success connection!");
+    }
 }
