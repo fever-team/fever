31b80f399398510fb523ade0be4056216570a574
Yu UiSeon <dmltjs851@gmail.com>
2020-06-08 05:17:36 +0900

    Update agent connection

--- fever-agent/src/main/java/com/fever/agent/AgentController.java
+++ fever-agent/src/main/java/com/fever/agent/AgentController.java
@@ -1,15 +1,23 @@
 package com.fever.agent;
 
+import java.net.Socket;
+
 public class AgentController {
 
     private Thread thread;
+    private String host = "http://localhost";
+    private int port = 9000;
 
     public void run() {
         while(true) {
             thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    System.out.println("Thread Start ! ");
+                    try(Socket socket = new Socket(host, port)) {
+                        // TODO : 구현
+                    }catch (Exception e) {
+                        // TODO : 구현
+                    }
                 }
             });
             thread.start();
