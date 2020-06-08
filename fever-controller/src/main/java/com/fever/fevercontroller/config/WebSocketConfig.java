e47c392ed5bf8a3ca8b6aac996fb98f49e2d1fcc
Yu UiSeon <dmltjs851@gmail.com>
2020-06-09 03:51:54 +0900

    Update WebSocketConfig
    - Update endpoint

--- fever-controller/src/main/java/com/fever/fevercontroller/config/WebSocketConfig.java
+++ fever-controller/src/main/java/com/fever/fevercontroller/config/WebSocketConfig.java
@@ -18,6 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").withSockJS();
+
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
     }
 }
