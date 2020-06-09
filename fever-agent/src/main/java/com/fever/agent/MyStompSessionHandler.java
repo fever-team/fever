f0b7451869c597f28207c1875206f793f0f77798
Yu UiSeon <dmltjs851@gmail.com>
2020-06-09 18:07:23 +0900

    Add MyStompSessionHandler
    - stomp event func

--- fever-agent/src/main/java/com/fever/agent/MyStompSessionHandler.java
+++ fever-agent/src/main/java/com/fever/agent/MyStompSessionHandler.java
@@ -1,4 +1,36 @@
 package com.fever.agent;
 
-public class MyStompSessionHandler {
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+
+import java.lang.reflect.Type;
+
+public class MyStompSessionHandler implements StompSessionHandler {
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        session.subscribe("/topic/greetings", this);
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return null;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+
+    }
 }
