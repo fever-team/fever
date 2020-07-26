5b89b1549e7561bc538a3eb58ceb892562c8ff62
Yu UiSeon <dmltjs851@gmail.com>
2020-07-26 15:13:52 +0900

    Add rabbit config
    * Add queue
    
    * Add exchange
    
    * Add binding

--- fever-controller/src/main/java/com/fever/fevercontroller/configuration/RabbitConfiguration.java
+++ fever-controller/src/main/java/com/fever/fevercontroller/configuration/RabbitConfiguration.java
@@ -1,5 +1,9 @@
 package com.fever.fevercontroller.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -9,6 +13,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfiguration {
+
+    public final static String FEVER_TPS_QUEUE = "fever-tps-queue";
+    public final static String FEVER_TPS_EXCHANGE = "fever-tps-exchange";
 
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
@@ -21,4 +28,20 @@ public class RabbitConfiguration {
     MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+    @Bean
+    Queue myQueue() {
+        return new Queue(FEVER_TPS_QUEUE);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(FEVER_TPS_EXCHANGE);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(FEVER_TPS_QUEUE);
+    }
+
 }
