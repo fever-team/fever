d86431ad47e5c892f94f746881091bd4290088d0
Yu UiSeon <dmltjs851@gmail.com>
2020-07-26 16:36:55 +0900

    Remove unused code

--- fever-agent/src/main/java/com/fever/agent/configuration/RabbitConfiguration.java
+++ fever-agent/src/main/java/com/fever/agent/configuration/RabbitConfiguration.java
@@ -1,9 +1,5 @@
 package com.fever.agent.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,25 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfiguration {
-
-    private static final String queueName = "q.fever";
-
-    private static final String topicExchangeName = "x.fever";
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-    }
 
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
