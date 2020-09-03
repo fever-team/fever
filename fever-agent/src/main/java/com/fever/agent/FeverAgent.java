1931995542243c9c235050191caed0365a07bd65
dmltjs851 <dmltjs851@gmail.com>
2020-09-04 01:52:28 +0900

    Add async config

--- fever-agent/src/main/java/com/fever/agent/FeverAgent.java
+++ fever-agent/src/main/java/com/fever/agent/FeverAgent.java
@@ -2,12 +2,9 @@ package com.fever.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class FeverAgent {
-
     public static void main(String[] args) {
         SpringApplication.run(FeverAgent.class, args);
     }
