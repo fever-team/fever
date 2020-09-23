3d2b0a2c0f660e69311593b0597b296b324de5d1
dmltjs851 <dmltjs851@gmail.com>
2020-09-24 00:45:06 +0900

    Add OAuthController for test

---
+++ auth/src/main/java/com/fever/auth/controller/OAuthController.java
@@ -0,0 +1,50 @@
+package com.fever.auth.controller;
+
+import com.fever.auth.model.OAuthToken;
+import com.google.gson.Gson;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestOperations;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value = "/oauth2")
+public class OAuthController {
+
+    @Autowired
+    private Gson gson;
+
+    @GetMapping(value = "/callback")
+    public OAuthToken callBackSocial(@RequestParam String code) {
+        String credentials = "testClient:testSecret";
+        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization", "Basic " + encodedCredentials);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("grant_type", "authorization_code");
+        params.add("redirect_uri", "http://localhost:8081/oauth2/callback");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/oauth/token", request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return gson.fromJson(response.getBody(), OAuthToken.class);
+        }
+        return null;
+    }
+
+}
